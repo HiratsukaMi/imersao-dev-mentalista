7751d9f7ae0dee934cdef8901991eae66fd3b15b
Michelly Hiratsuka Zorzi <43034694+HiratsukaMi@users.noreply.github.com>
2022-03-10 18:49:17 -0300

    Contagem de pontos de sanidade

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var numeroSecreto = 0;
 var elementoResultado = document.getElementById("resultado");
 var tentativas = 5;
+var pontosDeSanidade = 55;
+var pontosDeSanidadeRestantes = 0;
+var elementoPontosDeSanidade = document.getElementById("pontosDeSanidade");
 
 function Sortear(){
     numeroSecreto = parseInt(Math.random() * 11);
@@ -12,23 +15,31 @@ function Chutar(){
     if(chute > 10 || chute < 0){
         elementoResultado.innerHTML = "Você deve chutar um número de 0 a 10. O mestre vai deixar você chutar novamente sem penalidades."
     } else if(chute == numeroSecreto){
-        elementoResultado.innerHTML = "Você acertou! Ganhou " + chute + " pontos de sanidade. (Tentativas restantes: " + tentativas + ")";
+        pontosDeSanidadeRestantes = pontosDeSanidade + chute;
+        elementoResultado.innerHTML = "Você acertou! Ganhou " + chute + " pontos de sanidade. <p>Tentativas restantes: " + tentativas + "</p>";
+        elementoPontosDeSanidade.innerHTML = "Pontos de sanidade: " + pontosDeSanidadeRestantes;
     } else {
         --tentativas;
         if(chute > numeroSecreto){
-            elementoResultado.innerHTML = "Você errou, perdendo " + chute + " pontos de sanidade. O número que você procura é menor do que o seu chute. Tente novamente. (Tentativas restantes: " + tentativas + ")";
+            elementoResultado.innerHTML = "Você errou, perdendo " + chute + " pontos de sanidade. O número que você procura é menor do que o seu chute. Tente novamente. <p>Tentativas restantes: " + tentativas + "</p>";
         } else {
-            elementoResultado.innerHTML = "Você errou, perdendo " + chute + " pontos de sanidade. O número que você procura é maior do que o seu chute. Tente novamente. (Tentativas restantes: " + tentativas + ")";
+            elementoResultado.innerHTML = "Você errou, perdendo " + chute + " pontos de sanidade. O número que você procura é maior do que o seu chute. Tente novamente. <p>Tentativas restantes: " + tentativas + "</p>";
         }
+
+        pontosDeSanidade = pontosDeSanidade - chute;
+        pontosDeSanidadeRestantes = pontosDeSanidade;
+        elementoPontosDeSanidade.innerHTML = "Pontos de Sanidade: " + pontosDeSanidadeRestantes;
     } 
     
     if(tentativas == 0){
-        elementoResultado.innerHTML = "Seu número de tentativas acabou. Você enlouqueceu. (O número secreto é: " + numeroSecreto + ")";
+        elementoResultado.innerHTML = "Seu número de tentativas acabou. Você enlouqueceu. <p>O número secreto é: " + numeroSecreto + "</p>";
     }
 }
 
 function JogarNovamente() {
     Sortear();
     tentativas = 5;
-    elementoResultado.innerHTML = "Novo jogo. Sua sanidade está restaurada! (Número de tentativas: " + tentativas + ")";
+    pontosDeSanidade = 55;
+    elementoResultado.innerHTML = "Novo jogo. Suas tentativas e sanidade estão restauradas! <p>Número de tentativas: " + tentativas + "</p>";
+    elementoPontosDeSanidade.innerHTML = "Pontos de sanidade: " + pontosDeSanidade;
 }
